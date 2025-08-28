aece8655775d0fae2fb13dca4ac0fd79b2effd0c
jinia hossen <jiniahossen31@gmail.com>
2025-08-28 21:10:37 +0600

    variable

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-function abc(){
-    console.log(abc.xyz)
+// function abc(){
+//     console.log(abc.xyz)
+// }
+// abc()
+// abc.xyz=400;
+// abc.xyz=200;
+// abc();
+
+// variable -let,const,var
+/*************************************************/
+// var a = "Jinia";
+// var b = "Akter";
+// {
+//   var b = "Hira";
+//   console.log(b);
+// }
+// console.log(b);
+/***************************** ************************/
+let a="Jinia "
+let b="AKter"
+{
+    let b="Hira"
+    console.log(b)
 }
-abc()
-abc.xyz=400;
-abc.xyz=200;
-abc()
+console.log(b)
